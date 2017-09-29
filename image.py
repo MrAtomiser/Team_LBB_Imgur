8e97b35bd1f12e6e57008fb286e0363e1b57b00f
Sadiq Okocha <sokocha@gmail.com>
2017-09-29 19:13:53 +0000

    creates Image class

---
+++ image.py
@@ -0,0 +1,9 @@
+class Image():
+  def __init__(user_id, title, description, tags=[], image_gallery_id, score):
+    self.user_id = user_id
+    self.title = title
+    self.description = description
+    self.tags = tags
+    self.image_gallery_id = image_gallery_id
+    self.score = score
+
