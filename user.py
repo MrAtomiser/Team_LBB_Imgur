1385269b406b584e58ce1299351787ad1762f9ef
Sadiq Okocha <sokocha@gmail.com>
2017-09-29 19:08:42 +0000

    creates User class

---
+++ user.py
@@ -0,0 +1,7 @@
+class User():
+  def __init__(self, username, password, date_created, profile_image):
+    self.username = username
+    self. password = password
+    self.date_created = date_created
+    self.profile_image = profile_image
+
