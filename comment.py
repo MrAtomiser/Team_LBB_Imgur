405cda652285eea84413fe0dd04823e576b536df
Sadiq Okocha <sokocha@gmail.com>
2017-09-29 19:10:22 +0000

    creates Comment class

---
+++ comment.py
@@ -0,0 +1,6 @@
+class Comment(user, date_created, image_id, text, score):
+  self.user_id = user.id
+  self.date_created = date_created
+  self.image_id = image_id
+  self.text = text
+  self.score = score
