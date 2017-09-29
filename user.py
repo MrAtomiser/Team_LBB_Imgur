94a2afa1f7a8ca291e286b793baf9fc888f64744
Sadiq Okocha <sokocha@gmail.com>
2017-09-29 19:24:27 +0000

    seeds 5 users

--- user.py
+++ user.py
@@ -1,3 +1,5 @@
+import datetime 
+
 class User():
   def __init__(self, username, password, date_created, profile_image):
     self.username = username
@@ -5,3 +7,8 @@ class User():
     self.date_created = date_created
     self.profile_image = profile_image
 
+sadiq = User(username = "sadiq", password = "password", date_created="", profile_image="sadiqimage")
+tomi = User(username = "tomi", password = "password", date_created="", profile_image="tomiimage")
+abdul = User(username = "abdul", password = "password", date_created="", profile_image="abdulimage")
+dare = User(username = "dare", password = "password", date_created="", profile_image="dareimage")
+charles = User(username = "charles", password = "password", date_created="", profile_image="charlesimage")
