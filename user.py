4a7314df6708c43ac1bb1e790a75e7d964639d85
Sadiq Okocha <sokocha@gmail.com>
2017-09-30 14:47:41 +0000

    adds command line functionality for create_image

--- user.py
+++ user.py
@@ -11,10 +11,22 @@ class User():
     self.date_created = date_created
     self.profile_image = profile_image
 
+  def create_image(self):
+    user_id = input("Who's uploading the image? ")
+    title = input("What's the image title? ")
+    description = input("what the description? ")
+    tags = input("What are teh tags? ")
+    image_gallery_id = input("What gallery? ")
+    score = input("Score it")
+    new_image = Image(user_id,title,description,tags,image_gallery_id,score)
+
+
+
+
 sadiq = User(username = "sadiq", password = "password", date_created="", profile_image="sadiqimage")
 tomi = User(username = "tomi", password = "password", date_created="", profile_image="tomiimage")
 abdul = User(username = "abdul", password = "password", date_created="", profile_image="abdulimage")
 dare = User(username = "dare", password = "password", date_created="", profile_image="dareimage")
 charles = User(username = "charles", password = "password", date_created="", profile_image="charlesimage")
 
-
+sadiq.create_image()
