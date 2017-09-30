05c660251fa10fd50d2ae7c1d3d4aa2f34490e4c
Sadiq Okocha <sokocha@gmail.com>
2017-09-30 15:24:19 +0000

    successful created of read_images method

--- image.py
+++ image.py
@@ -1,11 +1,15 @@
 class Image():
-  def __init__(self,user_id, title, description, tags, image_gallery_id, score):
-    self.user_id = user_id
+  def __init__(self,title, description, tags, image_gallery_id, score):
     self.title = title
     self.description = description
     self.tags = tags
     self.image_gallery_id = image_gallery_id
     self.score = score
 
-    print("Image:{} successfully uploaded by {}".format(title,user_id))
+    print("Image: {} successfully uploaded".format(title))
+
+
+
+
+
 
--- user.py
+++ user.py
@@ -5,30 +5,38 @@ from image   import Image
 from image_gallery import ImageGallery
 
 class User():
-  def __init__(self, username, password, date_created, profile_image):
+  def __init__(self, username, password, date_created, profile_image, my_images = []):
     self.username = username
     self. password = password
     self.date_created = date_created
     self.profile_image = profile_image
+    self.my_images = my_images
 
 
   def create_image(self):
-    user_id = input("Who's uploading the image? ")
     title = input("What's the image title? ")
     description = input("what the description? ")
-    tags = input("What are teh tags? ")
+    tags = input("What are the tags? ")
     image_gallery_id = input("What gallery? ")
     score = input("Score it: ")
-    new_image = Image(user_id,title,description,tags,image_gallery_id,score)
+    new_image = Image(title,description,tags,image_gallery_id,score)
+    self.my_images.append(new_image)
+
+  def read_images(self):
+    for image in self.my_images:
+      print(image)
 
 
 
 
-sadiq = User(username = "sadiq", password = "password", date_created="", profile_image="sadiqimage")
-tomi = User(username = "tomi", password = "password", date_created="", profile_image="tomiimage")
-abdul = User(username = "abdul", password = "password", date_created="", profile_image="abdulimage")
-dare = User(username = "dare", password = "password", date_created="", profile_image="dareimage")
-charles = User(username = "charles", password = "password", date_created="", profile_image="charlesimage")
+
+
+sadiq = User(username = "sadiq", password = "password", date_created="", profile_image="sadiqimage",my_images = [])
+tomi = User(username = "tomi", password = "password", date_created="", profile_image="tomiimage",my_images = [])
+abdul = User(username = "abdul", password = "password", date_created="", profile_image="abdulimage",my_images = [])
+dare = User(username = "dare", password = "password", date_created="", profile_image="dareimage",my_images = [])
+charles = User(username = "charles", password = "password", date_created="", profile_image="charlesimage",my_images = [])
 
 sadiq.create_image()
+sadiq.read_images()
 
