97f4c4a35ab10fc9edf87396754e652e77676e70
{abdul} <{abdulquadrisotomiwa@gmail.com}>
2017-10-02 15:20:20 +0000

    I worked on creating new methods for Replya nd Comment

--- comment.py
+++ comment.py
@@ -1,9 +1,30 @@
-class Comment():
-  def __init__(self,user, date_created, image_id, text, score):
-    self.user = user
-    self.date_created = date_created
-    self.image_id = image_id
-    self.text = text
-    self.score = score
+# class Comment():
+#   def __init__(self,user, date_created, image_id, text, score):
+#     self.user = user
+#     self.date_created = date_created
+#     self.image_id = image_id
+#     self.text = text
+#     self.score = score
 
-  
+import time
+
+class Comment:
+  def __init__(self, message, sender_ID, points, post_ID,replies=[]):  # CommentID Needed?
+    self.message = message
+    self.timesent = time()
+    self.sender_ID = sender_ID
+    self.points = points
+    self.post_ID = post_ID
+    self.replies = replies
+
+  def edit_comment(self, new_comment):
+    self.new_comment = new_comment
+
+  def vote_comment(self):
+    pass
+
+  def delete_comment(self):
+    pass
+
+  def send_comment(self):
+    pass
